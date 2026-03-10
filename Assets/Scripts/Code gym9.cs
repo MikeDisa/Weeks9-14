469938229c58a19e3ec2b5ad9cd2528494c18f75
Mike DIsa <disa@sheridancollege.ca>
2026-03-10 11:50:08 -0400

    movement works, rotation does not

--- Assets/Scripts/Code gym9.cs
+++ Assets/Scripts/Code gym9.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Codegym9 : MonoBehaviour
 {
+    public float speed = 5;
+    public Vector2 movement;
+    public Vector3 rotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //I don't know what I'm doing
+        //Vector3 newRotation = transform.eulerAngles;
+        //newRotation.z += rotation.y * speed * Time.deltaTime;
+        //transform.eulerAngles += (Vector3)movement * speed * Time.deltaTime;
+        transform.eulerAngles = context.ReadValue<Vector2>();
     }
 }
