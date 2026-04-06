a600911c7d9ff8e9e7eeeeafbde8a9fa553303c7
MININT-5TERVH1\micha <disa@sheridancollege.ca>
2026-04-06 18:06:53 -0400

    got the unity event set up.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public int hitPoints = 3;
     public int block = 1;
+
+    public float fireCooldown = 0.3f;
+    float nextFireTime = 0f;
 
     public float speed = 5;
     public Vector2 movement;
@@ -18,10 +22,19 @@ public class Player : MonoBehaviour
     public SpriteRenderer UfoRenderer;
 
     public Shieldfunction shield;
+
+    public UnityEvent onAttackEvent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentPos = transform.position;
+
+        if (onAttackEvent == null)
+        {
+            onAttackEvent = new UnityEvent();
+        }
+        onAttackEvent.RemoveListener(FireShot);
+        onAttackEvent.AddListener(FireShot);
     }
 
     // Update is called once per frame
@@ -29,16 +42,16 @@ public class Player : MonoBehaviour
     {
         currentPos = transform.position;
 
-        transform.position += new Vector3(movement.x * speed * Time.deltaTime, 0f,0f);
+        transform.position += new Vector3(movement.x * speed * Time.deltaTime, 0f, 0f);
 
         if (transform.position.x <= -11.55f)
         {
-            transform.position = new Vector3 (-11.55f,currentPos.y,0f);
+            transform.position = new Vector3(-11.55f, currentPos.y, 0f);
         }
-       
+
         if (transform.position.x >= 11.55f)
         {
-            transform.position = new Vector3 (11.55f,currentPos.y,0f);
+            transform.position = new Vector3(11.55f, currentPos.y, 0f);
         }
         //transform.position = new Vector2 (movement.x,transform.position.y);
     }
@@ -52,43 +65,57 @@ public class Player : MonoBehaviour
     public void DamagePlayer(int dmg)
     {
         hitPoints = hitPoints - dmg * block;
-        if (hitPoints == 0)
+        if (hitPoints <= 0)
         {
-            //Debug.Log("hit");
+            Debug.Log("hit");
         }
     }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if (context.phase == InputActionPhase.Performed)
+        if (context.performed)
         {
-            if (PlayerShotPrefab == null || ufo == null || UfoRenderer == null)
-            {
-                //Debug.LogWarning("Projectile prefab or Ufo references missing.");
-                return;
-            }
-            Vector2 offset = new Vector2(currentPos.x+1,currentPos.y+3);
-            GameObject newProjectile = Instantiate(PlayerShotPrefab, offset, Quaternion.identity);
+            onAttackEvent.Invoke();
+        }
+    }
 
-            // Get the projectile script from the spawned prefab
-            PlayerShot shotScript = newProjectile.GetComponent<PlayerShot>();
+    public void FireShot()
+    {
+        if (Time.time < nextFireTime)
+        {
+            return;
+        }
 
-            if (shotScript != null)
-            {
-                shotScript.ufo = ufo;
-                shotScript.ufoRenderer = UfoRenderer;
+        nextFireTime = Time.time + fireCooldown;
 
-            }
+        if (PlayerShotPrefab == null || ufo == null || UfoRenderer == null)
+        {
+            return;
+        }
+
+        Vector2 offset = new Vector2(currentPos.x + 1, currentPos.y + 3);
+        GameObject newProjectile = Instantiate(PlayerShotPrefab, offset, Quaternion.identity);
+
+        // Get the projectile script from the spawned prefab
+        PlayerShot shotScript = newProjectile.GetComponent<PlayerShot>();
+
+        if (shotScript != null)
+        {
+            shotScript.ufo = ufo;
+            shotScript.ufoRenderer = UfoRenderer;
+
         }
     }
 
     public void OnShield(InputAction.CallbackContext context)
     {
-       
+
         if (context.performed)
-        { Debug.Log("click" + context.phase);
+        {
+            Debug.Log("click" + context.phase);
             shield.gameObject.SetActive(true);
             block = 0;
+            onAttackEvent.RemoveListener(FireShot);
         }
 
         if (context.canceled)
@@ -96,6 +123,8 @@ public class Player : MonoBehaviour
             Debug.Log("click" + context.phase);
             block = 1;
             shield.gameObject.SetActive(false);
+            onAttackEvent.RemoveListener(FireShot);
+            onAttackEvent.AddListener(FireShot);
         }
     }
 }
