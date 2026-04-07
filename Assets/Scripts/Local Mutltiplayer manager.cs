88c156ca1b5c19051e10ca7ed5abadfca244146c
Mike Disa <disa@sheridancollege.ca>
2026-04-07 10:02:01 -0400

    local multipayer set up

---
+++ Assets/Scripts/Local Mutltiplayer manager.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class LocalMutltiplayermanager : MonoBehaviour
+{
+    
+    public List<Sprite> playerSprites;
+    public List<PlayerInput> players;
+
+    public void OnPlayerJoined(PlayerInput player)
+    {
+        players.Add(player);
+
+        SpriteRenderer sr = player.GetComponent<SpriteRenderer>();
+        sr.sprite = playerSprites[player.playerIndex];
+
+        LocalMulitplayerControler controller = player.GetComponent<LocalMulitplayerControler>();
+        controller.manager = this;
+    }
+
+    public void PlayerAttacking(PlayerInput attackPlayer)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (attackPlayer == players[i]) continue;
+
+           if( Vector2.Distance(attackPlayer.transform.position, players[i].transform.position) < 0.5f)
+           {
+                Debug.Log("Player " + attackPlayer.playerIndex + "hit player " + players[i].playerIndex);
+           }
+        }
+    }
+}
